34ecd8c86893e89aa0dda6990fd03ad58467d021
kavvz20 <kavyathukral20@gmail.com>
2026-02-23 23:16:15 +0530

    Fix CORS + endpoint

--- src/components/PredictionForm.jsx
+++ src/components/PredictionForm.jsx
@@ -49,7 +49,7 @@ function PredictionForm() {
     setLoading(true);
     setResult(null);
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://churn-2bza.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
